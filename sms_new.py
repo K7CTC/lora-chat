7d6445b5bd3dcba969c68059aa5807fa29eec740
Chris Clement <48072888+K7CTC@users.noreply.github.com>
2020-11-08 16:41:01 -0700

    v0.8

---
+++ sms_new.py
@@ -0,0 +1,159 @@
+########################################################################
+#                                                                      #
+#          NAME:  LoRa Chat - New SMS                                  #
+#  DEVELOPED BY:  Chris Clement (K7CTC)                                #
+#       VERSION:  v0.8                                                 #
+#   DESCRIPTION:  This module reads lora_chat.conf to obtain the node  #
+#                 identifier and validates a user provided message of  #
+#                 up to 50 characters. An SMS packet type identifier   #
+#                 is appended and the resulting data is inserted into  #
+#                 a new row within the sms table of lora_chat.db.      #
+#                                                                      #
+########################################################################
+
+import argparse
+import os
+import re
+import sqlite3
+import sys
+import time
+from pathlib import Path
+
+my_node_id = None
+my_node_name = None
+
+#establish and parse command line arguments
+parser = argparse.ArgumentParser(description='LoRa Chat - New SMS',
+                                 epilog='Created by K7CTC. This module reads lora_chat.conf to '
+                                        'obtain the node identifier and validates a user provided '
+                                        'message of up to 50 characters. An SMS packet type '
+                                        'identifier is appended and the resulting data is '
+                                        'inserted into a new row within the sms table of '
+                                        'lora_chat.db.')
+parser.add_argument('-m', '--message', nargs='?', default=None,
+                    help='message of up to 50 characters in length to be queued for transmission')
+args = parser.parse_args()
+
+if Path('lora_chat.db').is_file() == False:
+    print('ERROR: File not found - lora_chat.db')
+    sys.exit(1)
+
+if Path('lora_chat.conf').is_file() == False:
+    print('ERROR: File not found - lora_chat.conf')
+    sys.exit(1)
+
+#attempt to read and validate the node id integer from lora_chat.conf
+try:
+    file = open('lora_chat.conf')
+    my_node_id = int(file.readline())
+    file.close()
+except:
+    print('ERROR: Failed to read node identifier from lora_chat.conf!')
+    sys.exit(1)
+if my_node_id < 1 or my_node_id > 99:
+    print('ERROR: Node identifier out of range!')
+    sys.exit(1)
+
+#use node id to obtain corresponding node name from the database
+try:
+    db = sqlite3.connect('lora_chat.db')
+    c = db.cursor()
+    c.execute('SELECT node_name FROM nodes WHERE node_id=?',(my_node_id,))
+    query_result = c.fetchone()
+    if query_result:
+        my_node_name = query_result[0]
+    else:
+        print('ERROR: Invalid node identifier!')
+        c.close()
+        db.close()
+        sys.exit(1)
+except:
+    print('ERROR: Unable to connect to lora_chat.db') 
+    c.close()
+    db.close()
+    sys.exit(1)
+
+#function: message validation, returns boolean
+def validate_message(message_to_be_validated):
+    #only contain A-Z a-z 0-9 . ? ! and between 1 and 50 chars in length
+    if re.fullmatch('^[a-zA-Z0-9!?. ]{1,50}$', message_to_be_validated):
+        return True
+    else:
+        return False
+
+#function: insert message into database, returns boolean  
+def database_entry(message):
+    #compose raw packet to be sent over the air
+    payload_raw = str(1) + ',' + str(my_node_id) + ',' + message
+    #compose hex encoded version of raw packet to be sent over the air
+    payload_hex = payload_raw.encode('UTF-8').hex()
+    #attempt database entry
+    try:
+        db = sqlite3.connect('lora_chat.db')
+        c = db.cursor()
+        #enable foreign key constraints
+        c.execute('PRAGMA foreign_keys = ON')
+        time_queued = int(round(time.time()*1000))
+        c.execute('''
+            INSERT INTO sms (
+                node_id,
+                message,
+                payload_raw,
+                payload_hex,
+                time_queued)
+            VALUES (?, ?, ?, ?, ?);''',
+            (my_node_id, message, payload_raw, payload_hex, time_queued))
+    except:
+        c.close()
+        db.close()
+        return False
+    else:
+        db.commit()
+        c.close()
+        db.close()
+        return True
+
+#function: clear terminal
+def clear_terminal():
+    if sys.platform == 'win32':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+#if message provided via command line is valid then insert into database
+if args.message != None:
+    if validate_message(args.message):
+        #message is valid, insert into database then quit
+        if database_entry(args.message):
+            print('SUCCESS: Message has been queued for transmission.')
+            sys.exit(0)
+        else:
+            print('ERROR: Database entry failure!')
+            sys.exit(1)
+    else:
+        print('ERROR: Your message contained invalid characters or is of invalid length!')
+        sys.exit(1)
+
+#new sms loop
+if args.message == None:
+    while True:
+        try:
+            clear_terminal()
+            print('┌───────────┤Experimental LoRa Mesh Messenger - New SMS├───────────┐')
+            print('│ Type a message between 1 and 50 characters then press enter.     │')
+            print('│ Your message may only contain A-Z a-Z 0-9 and the following      │')
+            print('│ special characters: period, question mark and exclamation mark   │')
+            print('└──────────────────────────────────────────────────────────────────┘')
+            message = input(my_node_name + ': ')
+            if validate_message(message):
+                if database_entry(message):
+                    print()
+                    print('SUCCESS: Message has been queued for transmission.')
+                    time.sleep(5)
+            else:
+                print()
+                print('ERROR: Your message contained invalid characters or is of invalid length!')
+                time.sleep(5)
+        except KeyboardInterrupt:
+            print()
+            sys.exit(0)
---
+++ sms_view.py
@@ -0,0 +1,116 @@
+########################################################################
+#                                                                      #
+#          NAME:  LoRa Chat - View SMS                                 #
+#  DEVELOPED BY:  Chris Clement (K7CTC)                                #
+#       VERSION:  v0.8                                                 #
+#   DESCRIPTION:  This module reads the sms table from lora_chat.db    #
+#                 and prints the output to the console.                #
+#                                                                      #
+########################################################################
+
+import datetime
+import sqlite3
+import sys
+import time
+from pathlib import Path
+
+my_node_id = None
+
+if Path('lora_chat.db').is_file() == False:
+    print('ERROR: File not found - lora_chat.db')
+    sys.exit(1)
+
+if Path('lora_chat.conf').is_file() == False:
+    print('ERROR: File not found - lora_chat.conf')
+    sys.exit(1)
+
+#attempt to read and validate the node id integer from lora_chat.conf
+try:
+    file = open('lora_chat.conf')
+    my_node_id = int(file.readline())
+    file.close()
+except:
+    print('ERROR: Failed to read node identifier from lora_chat.conf!')
+    sys.exit(1)
+if my_node_id < 1 or my_node_id > 99:
+    print('ERROR: Node identifier out of range!')
+    sys.exit(1)
+
+rowid_marker = 0
+
+db = sqlite3.connect('lora_chat.db')
+c = db.cursor()
+while True:
+    try:    
+        #get all rows with rowid greater than rowid_marker
+        c.execute('''
+            SELECT
+                sms.rowid,
+                sms.node_id,
+                nodes.node_name,
+                sms.message,
+                sms.time_sent,
+                sms.air_time,
+                sms.time_received,
+                sms.rssi,
+                sms.snr
+            FROM
+                sms
+            NATURAL JOIN
+                nodes
+            WHERE
+                sms.rowid>?;''',
+                (rowid_marker,))
+        for record in c.fetchall():
+            #if record is lacks time_sent and time_received update rowid_marker and break
+            if record[4] == None and record[6] == None:
+                rowid_marker = int(record[0]) - 1
+                break             
+            print()
+            if record[1] == my_node_id:
+                unix_time_sent = int(record[4]) / 1000
+                time_sent = datetime.datetime.fromtimestamp(unix_time_sent).strftime('%I:%M:%S %p')
+                message_length = len(record[3])
+                airtime_length = len(str(record[5]))
+                right_align_air_time_whitespace = ' ' * (54 - airtime_length)
+                if message_length <= 11:
+                    right_align_whitespace = ' ' * 53
+                    message_padding = ' ' * (11 - message_length)
+                    print(f'{right_align_whitespace}┌─────────────┐')
+                    print(f'{right_align_whitespace}│ {message_padding}{record[3]} │')
+                    print(f'{right_align_whitespace}└┤{time_sent}├┘')
+                    print(f'{right_align_air_time_whitespace}(Air Time: {record[5]}ms)')
+                else:
+                    right_align_length = 64 - message_length
+                    right_align_whitespace = ' ' * right_align_length
+                    border_top = '─' * message_length
+                    border_bottom = '─' * (message_length - 12)
+                    print(f'{right_align_whitespace}┌─{border_top}─┐')
+                    print(f'{right_align_whitespace}│ {record[3]} │')
+                    print(f'{right_align_whitespace}└─{border_bottom}┤{time_sent}├┘')
+                    print(f'{right_align_air_time_whitespace}(Air Time: {record[5]}ms)')
+            else:
+                unix_time_received = int(record[6]) / 1000
+                time_received = datetime.datetime.fromtimestamp(unix_time_received).strftime('%I:%M:%S %p')
+                message_length = len(record[3])
+                if message_length <= 11:
+                    message_padding = ' ' * (11 - message_length)
+                    print(f'┌─────────────┐')
+                    print(f'│ {record[3]}{message_padding} │')
+                    print(f'└┤{time_received}├┘')
+                    print(f'{record[2]} (RSSI: {str(record[7])}   SNR: {str(record[8])})')
+                else:
+                    border_top = '─' * message_length
+                    border_bottom = '─' * (message_length - 12)
+                    print(f'┌─{border_top}─┐')
+                    print(f'│ {record[3]} │')
+                    print(f'└┤{time_received}├{border_bottom}─┘')
+                    print(f'{record[2]} (RSSI: {str(record[7])}   SNR: {str(record[8])})')
+            rowid_marker = record[0]
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print()
+        break
+c.close()
+db.close()
+sys.exit(0)
